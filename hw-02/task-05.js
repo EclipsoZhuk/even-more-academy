f7629edef3323b41be2b0c1d9229569abd12e857
Vlad Gonchar <78562749+EclipsoZhuk@users.noreply.github.com>
2022-02-03 07:29:54 +0200

    completed hw-02

---
+++ hw-02/task-05.js
@@ -0,0 +1,6 @@
+// Task 5
+
+Function.prototype.call = function (context, ...args) {
+    // TODO
+    return this.apply(context, args);
+};
---
+++ hw-02/task-07.js
@@ -0,0 +1,11 @@
+// Task 7
+
+const greet_abe = function () {
+    const name = 'Abe';
+    return 'Hello, ' + name + '!';
+};
+
+const greet_ben = function () {
+    const name = 'Ben';
+    return 'Hello, ' + name + '!';
+};
